a9797238c8c75bec7d8012714ef062db3e39bf61
enricoianni <48230383+enricoianni@users.noreply.github.com>
2022-07-29 13:41:29 +0800

    Add files via upload

--- FTIR/First_FTIR_DPT_files_to_CSV_WebApp.py
+++ FTIR/First_FTIR_DPT_files_to_CSV_WebApp.py
@@ -1,3 +1,4 @@
+import io
 class bcolors:
     HEADER = '\033[95m'
     OKBLUE = '\033[94m'
@@ -123,20 +124,14 @@ def get_value(df, label):
     return final_val
 
 
-def sed3(fc):
+def sed4(df_uploader2):
     global res, files
+    input_files = list(df_uploader2.value.values())
     files = []
-    for (root, dirs, file) in os.walk(Path(fc.selected_path)):
-        # Skip the ipynb files folder"
-        if root.endswith('ipynb_checkpoints'):
-            continue
-        for f in file:
-            if ((f.endswith('.dpt') | f.endswith('.0') | f.endswith('.1')) and (f.split('.')[0])!='NBG'):  # if f.endswith('.dpt'):
-                files.append(f)
+    for f in input_files:
+        if ((f['metadata']['name'].endswith('.dpt') | f['metadata']['name'].endswith('.0') | f['metadata']['name'].endswith('.1')) and (f['metadata']['name'].split('.')[0])!='NBG'):
+            files.append(f['metadata']['name'])
 
-    # res = pd.DataFrame(columns=['Sample_ID', 'Clay_wt%', 'Qtz_wt%',
-    #                             'Carb_wt%','TOC_wt%',
-    #                             'Gibbs_indx', 'TSGVersion'])
     res = pd.DataFrame(columns=['Sample_ID', 'Clay_abun', 'Qtz_abun',
                                 'Carb_abun', 'TOC',
                                 'Gibbs_abun', 'TSGversion'])
@@ -175,16 +170,16 @@ def sed3(fc):
         # os.mkdir('./FTIR/output/DPT')
         # # ('./FTIR/output').mkdir()
 
-        for file in files:
-            df = pd.read_csv(Path(fc.selected_path).as_posix() + '/' + file,
-                             header=None)  # df = pd.read_csv(path + file, header=None)
+        for file in input_files:
+            decod=io.StringIO(file['content'].decode('utf8'))
+            df = pd.read_csv(decod, header=None) #pd.read_csv(Path(fc.selected_path).as_posix() + '/' + file, header=None)  # df = pd.read_csv(path + file, header=None)
             df.columns = ['wavenumber', 'intensity']
             df = df.set_index('wavenumber').sort_index()
             #         print(df)
             wavenumber = list(df.index)
             #         print(df)
             for mineral in res.columns:
-                res.loc[file.split('.')[0], mineral] = get_value(df,
+                res.loc[file['metadata']['name'].split('.')[0], mineral] = get_value(df,
                                                                  mineral)  # res.loc[file[:-4], mineral] = get_value(df, mineral)
             # #copy over DPT files
             # shutil.copy(Path(fc.selected_path) / file, './FTIR/output/DPT') #shutil.copy('./FTIR/output/DPT/'+ file, Path(fc.selected_path)) #shutil.copy('./FTIR/output/DPT' / file, Path(fc.selected_path))
@@ -202,7 +197,7 @@ def sed3(fc):
             print('List of samples: {} '.format(res2[res2['Clay_abun'] < 30].index.values.tolist()))
             display(res2[res2['Clay_abun'] < 30])
         res2['total_sum'] = res2['Clay_abun'] + res2['Qtz_abun'] + res2['Carb_abun'] + res2['TOC']
-        if len(res2['total_sum'] < 40) > 1:
+        if len(res2[res2['total_sum'] < 40]) > 1:
             print(
                 f"{bcolors.BOLD}{bcolors.FAIL}Warning: The samples listed below have the sum of 'Clay_abun', 'Qtz_abun', 'Carb_abun' and 'TOC' lower than 40%. The measurements of these samples needs to be repeated on a new fresh sample.{bcolors.ENDC}")
             print('List of samples: {} '.format(res2[res2['total_sum'] < 40].index.values.tolist()))
@@ -222,7 +217,7 @@ def sed3(fc):
         # #make a csv file of dataframe res
         # res.to_csv('./FTIR/output/' + Path(fc.selected_path).name + '.csv', index=True) #res.to_csv('./FTIR/output/results.csv', index=True)  # res.to_csv('./output/results.csv', index=True)
         #save this csv also in the selected DPT folder
-        res.to_csv(Path(fc.selected_path) / (Path(fc.selected_path).name + '.csv'), index=True)
+        res.to_csv(('FTIR_results.csv'), index=True)
         #df.to_csv(file_path / 'working' / (file.name[:-12] + '.csv'), index=False)
         print(f"{bcolors.OKGREEN}Successfully executed!{bcolors.ENDC}")
         # print(res)
@@ -231,18 +226,15 @@ def sed3(fc):
         res = pd.DataFrame()
 
 
-# Create and display a FileChooser widget
-fc = FileChooser() #'C:/') #SED_source)  # W:/Logista LMS/Logista LMS Shared/Final reports/NIR Results #os.path.normpath
-
-# Switch to folder-only mode
-fc.show_only_dirs = True
-
-# Change the title (use '' to hide)
-fc.title = '<b>Select DPT folder</b>'
-
+#upload dpt files
+df_uploader2 =widgets.FileUpload(
+    accept='*.csv',  # Accepted file extension e.g. '.txt', '.pdf', 'image/*', 'image/*,.pdf'
+    multiple=True,  # True to accept multiple files upload else False
+    description="Upload_dpt(s)"
+)
 
 def main3(b):
-    sed3(fc=fc)
+    sed4(df_uploader2=df_uploader2)
 
 
 start_button = widgets.ToggleButton(
@@ -255,5 +247,5 @@ start_button = widgets.ToggleButton(
 )
 
 start_button.observe(main3, "value")
-display(fc)
+display(df_uploader2)
 display(start_button)
